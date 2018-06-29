6c34df5635fad9d28f8e95c777985d4d51134922
Oscar <oscar.infos@gmail.com>
2018-06-29 16:58:33 +0000

    schema validation works with schema from flask marshmallow

--- tests/contract/user-model-test.js
+++ tests/contract/user-model-test.js
@@ -1,7 +1,8 @@
 import { module, test } from 'qunit';
 // import request from 'ember-ajax/request';
 import ZSchema from 'z-schema';
-import { waitUntil } from '@ember/test-helpers'
+
+// import { waitUntil } from '@ember/test-helpers'
 // /*global server */
 // import { setupApplicationTest } from 'ember-qunit';
 
@@ -34,40 +35,27 @@ module('Unit | Model | user', function() {
 
   test('test the schema for user model',async function(assert) {
     
+    assert.expect(1)
+    
     var validator = new ZSchema({
         noEmptyArrays: true
     });
     
     
-    // var json = {};
-    // var schema = { "$ref": "http://json-schema.org/draft-04/schema#" };
-    
-    // var valid = validator.validate(json, schema);
-    
-    
-
     let schemaUrl = 'https://flask-fuhrman.c9users.io/';
-    
-    
-    
-    await waitUntil(()=>
-        getSchema(schemaUrl).then((response)=>{
-            // console.log(response.responseBody);
-            let schema = response.responseBody;
-            var valid = validator.validate(schema, { "type": "number" });
-            assert.ok(valid, "validate user schema");
-        },(error)=>{
-            assert.ok(false,"validate user schema: " + error)
-        })
-    )
-    // request('https://flask-fuhrman.c9users.io/',{}).then(response => {
-        
-    // });
-    
 
-        
-    
-    
+    await getSchema(schemaUrl).then((response)=>{
+            let schema = response;
+            let valid = validator.validate({"required": [],"id": 12, "name": "oscar", "other": 5}, schema);
+            let errors = validator.getLastErrors();
+            let msg = ""
+            if (errors !== undefined){
+              msg = errors[0].message
+            }
+            assert.ok(valid, "validate user schema: " + msg);
+        },(error)=>{
+            assert.ok(false,"get user schema: " + error)
+        })
     
     
     
